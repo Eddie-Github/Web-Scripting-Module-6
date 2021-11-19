d929c2d06597b76c66f5ee4595d5bf6aece98bdb
Eddie-Github <50275802+Eddie-Github@users.noreply.github.com>
2021-11-18 20:04:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//add the submit event listener to the form
+document.getElementById("salesTaxForm").addEventListener("submit", salesTaxForm);
+//form event
+function salesTaxForm(event) 
+{
+    // preventDefault method prevents the browser from clearing user-input entries when the user submits the form
+    event.preventDefault();
+    //create sales tax class and pass in amount
+    var amount = document.getElementById("amount").value;
+    var tax = new SalesTax(amount);
+    //add rate to the object
+    var rate = document.getElementById("rate").value;
+    tax.taxRate(rate);
+    //calculate tax and show output
+    document.getElementById("output").innerHTML = tax.calculate();
+}
+
+//sales tax calculator class
+class SalesTax{
+    //constructor with parameter for purchase amount
+    constructor(purchaseAmount){
+        this.purchaseAmount = purchaseAmount;
+    }
+    //method for adding tax rate
+    taxRate(rate){
+        this.rate = rate;
+    }
+    //calculate output
+    calculate(){
+        var taxAmount = parseFloat(this.purchaseAmount) * parseFloat(this.rate);
+        var total = parseFloat(this.purchaseAmount) + taxAmount;
+        return "The tax amount is: $" + taxAmount.toFixed(2) + 
+            "\nThe total with tax is: $" + total.toFixed(2);
+    }
+}
